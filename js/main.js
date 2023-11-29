70009e866cacf96df0c8f830cded9a85dfb391af
Yasi <113851370+YasiruBasura@users.noreply.github.com>
2023-11-30 03:20:36 +0530

    ..

--- js/main.js
+++ js/main.js
@@ -41,12 +41,16 @@ ScrollReveal({
 });
 
 //Target elements, and specify options to create reveal animations
+ScrollReveal().reveal('.home .info h2, .section-title-01, section-title-02',{delay: 500, origin:'left'});  
 ScrollReveal().reveal('.home .info h2',{delay: 500, origin:'left'});
-ScrollReveal().reveal('.home .info h3, .home .info p',{delay: 600, origin:'right'});
+ScrollReveal().reveal('.home .info h3, .home .info p, .about-info .btn',{delay: 600, origin:'right'});
 ScrollReveal().reveal('.home .info h2',{delay: 500, origin:'left'});
 ScrollReveal().reveal('.home .info .btn',{delay: 700, origin:'bottom'});
 ScrollReveal().reveal('.media-icons i',{delay: 500, origin:'left',interval:200});  //the interval option is used to delay the reveal of each icon
-ScrollReveal().reveal('.home-img',{delay: 500, origin:'bottom'});
+ScrollReveal().reveal('.home-img, about-img',{delay: 500, origin:'bottom'});
+ScrollReveal().reveal('.about .description',{delay: 500, origin:'left'});
+ScrollReveal().reveal('.about .professional-list li',{delay: 500, origin:'right', interval: 200});
+ScrollReveal().reveal('.portfolio .img-card-container .img-card',{delay: 800, origin:'bottom', interval: 200});
 
 
 
